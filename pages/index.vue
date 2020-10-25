10b27c7fc33227cb7fd5c3290693f75ad70c8073
miss tong <1962788480@qq.com>
2020-10-25 15:20:28 +0800

    basic structure

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">首页</div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {},
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted(){},
+    methods: {}
+}
+</script>
