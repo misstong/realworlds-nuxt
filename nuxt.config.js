9945fed03783c61af07bb4f0efa94c43dd73d4ec
miss tong <1962788480@qq.com>
2020-10-25 17:19:10 +0800

    login

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,58 @@
 module.exports = {
-    
+    router: {
+        linkActiveClass: 'active',
+        extendRoutes(routes, resolve) {
+            console.log(routes)
+            // routes.push({
+            //     name: 'custom',
+            //     path: '*',
+            //     componet
+            // })
+            console.log(__dirname,resolve(__dirname,'pages/layout'))
+            routes.splice(0)
+            routes.push(...[
+                {
+                    path: '/',
+                    component: resolve(__dirname, 'pages/layout'),
+                    children: [
+                        {
+                            path: '',
+                            name: 'home',
+                            component: resolve(__dirname, 'pages/home')
+                        },
+                        {
+                            path: 'login',
+                            name: 'login',
+                            component: resolve(__dirname, 'pages/login')
+                        },
+                        {
+                            path: 'register',
+                            name: 'register',
+                            component: resolve(__dirname, 'pages/login')
+                        },
+                        {
+                            path: 'profile/:username',
+                            name: 'profile',
+                            component: resolve(__dirname, 'pages/profile')
+                        },
+                        {
+                            name: 'settings',
+                            path: 'settings',
+                            component: resolve(__dirname, 'pages/settings')
+                        },
+                        {
+                            name: 'editor',
+                            path: 'editor',
+                            component: resolve(__dirname, 'pages/editor')
+                        },
+                        {
+                            name: 'article',
+                            path: 'article/:slug',
+                            component: resolve(__dirname, 'pages/article')
+                        }
+                    ]
+                }
+            ])
+        }
+    }
 }
--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -6,28 +6,47 @@
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
-            <a class="nav-link active" href="">Home</a>
+            <!-- <a class="nav-link active" href="">Home</a> -->
+            <nuxt-link class="nav-link" exact to="/">Home</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">
+            <!-- <a class="nav-link" href="">
               <i class="ion-compose"></i>&nbsp;New Post
-            </a>
+            </a> -->
+            <nuxt-link class="nav-link" to="/editor">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">
+            <!-- <a class="nav-link" href="">
               <i class="ion-gear-a"></i>&nbsp;Settings
-            </a>
+            </a> -->
+            <nuxt-link class="nav-link" to="/settings">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Sign up</a>
+            <!-- <a class="nav-link" href="">Sign up</a> -->
+            <nuxt-link class="nav-link" to="/register">Sign up</nuxt-link>
           </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="">Sign up</a> -->
+            <nuxt-link class="nav-link" to="/login">Sign in</nuxt-link>
+          </li>
+
+          <li class="nav-item">
+              <nuxt-link class="nav-link" to="profile/1">
+                <img class="user-pic">tangt
+              </nuxt-link>
+        </li>
         </ul>
       </div>
     </nav>
     <nuxt-child/>
     <footer>
       <div class="container">
-        <a href="/" class="logo-font">conduit</a>
+        <!-- <a href="/" class="logo-font">conduit</a> -->
+        <nuxt-link to="/" class="logo-font">conduit</nuxt-link>
         <span class="attribution">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
